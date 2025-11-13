bd2a7ad2260dec68beb45b7014eb76fdb5a6d8de
AndresSanchez <sanchezandres8203@gmail.com>
2025-11-13 00:08:29 -0400

    feat: backend connection login-signup

---
+++ lib/core/network/api_config.dart
@@ -0,0 +1,6 @@
+class ApiConfig {
+  static const String baseUrl = 'http://192.168.1.21:3333/api';
+  
+  static const Duration connectTimeout = Duration(seconds: 10);
+  static const Duration receiveTimeout = Duration(seconds: 10);
+}
